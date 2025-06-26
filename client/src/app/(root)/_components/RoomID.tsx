8cd358fee41f86a99a1843b3956504746868ca6a
Abhijeet-Junwal <abhijeetjunwal@gmail.com>
2025-06-26 16:31:47 +0530

    Setup id uuidv4

--- client/src/app/(root)/_components/RoomID.tsx
+++ client/src/app/(root)/_components/RoomID.tsx
@@ -1,53 +1,59 @@
 "use client";
-import { useState } from "react";
+import React,{ useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function RoomID() {
-     const [roomId, setRoomId] = useState('');
+    const [roomId, setRoomId] = useState('');
     const [username, setUsername] = useState('');
 
+   const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    setRoomId(id);    
+   }
 
-  return (
-  <>
-    <div className="homePageWrapper">
-            <div className="formWrapper">
-                
-                <h1 className="text-3xl font-bold p-4 m-2 mt-0 ml-0 pl-0">Collab Code Editor</h1>
-                <h4 className="mainLabel">Paste invitation ROOM ID</h4>
-                <div className="inputGroup ">
-                    <input
-                        type="text"
-                        className="inputBox text-black"
-                        placeholder="ROOM ID"
-                        onChange={(e) => setRoomId(e.target.value)}
-                        value={roomId}
-                        // onKeyUp={handleInputEnter}
-                    />
-                    <input
-                        type="text"
-                        className="inputBox text-black"
-                        placeholder="USERNAME"
-                        onChange={(e) => setUsername(e.target.value)}
-                        value={username}
-                        // onKeyUp={handleInputEnter}
-                    />
-                    <button className="btn joinBtn text-black">
-                        Join
-                    </button>
-                    <span className="createInfo">
-                        If you don't have an invite then create &nbsp;
-                        <a
-                            // onClick={createNewRoom}
-                            href=""
-                            className="createNewBtn"
-                        >
-                            new room
-                        </a>
-                    </span>
+    return (
+    <>
+        <div className="homePageWrapper">
+                <div className="formWrapper">
+                    
+                    <h1 className="text-3xl font-bold p-4 m-2 mt-0 ml-0 pl-0">Collab Code Editor</h1>
+                    <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+                    <div className="inputGroup ">
+                        <input
+                            type="text"
+                            className="inputBox text-black"
+                            placeholder="ROOM ID"
+                            onChange={(e) => setRoomId(e.target.value)}
+                            value={roomId}
+                            // onKeyUp={handleInputEnter}
+                        />
+                        <input
+                            type="text"
+                            className="inputBox text-black"
+                            placeholder="USERNAME"
+                            onChange={(e) => setUsername(e.target.value)}
+                            value={username}
+                            // onKeyUp={handleInputEnter}
+                        />
+                        <button className="btn joinBtn text-black">
+                            Join
+                        </button>
+                        <span className="createInfo">
+                            If you don't have an invite then create &nbsp;
+                            <a
+                                onClick={createNewRoom}
+                                href=""
+                                className="createNewBtn"
+                            >
+                                new room
+                            </a>
+                        </span>
+                    </div>
                 </div>
-            </div>
-        </div>
-  </>
-  )
+            </div> 
+    </>
+    )
 }
 
 export default RoomID;
