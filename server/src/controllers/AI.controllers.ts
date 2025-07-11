118dcae1fa59cb9d05e0bf73cff2c58185090d53
Rohan Bairagi <143432752+ROHANBAIRAGI123@users.noreply.github.com>
2025-07-11 22:50:36 +0530

    feat: add ai to contextMenu

--- server/src/controllers/AI.controllers.ts
+++ server/src/controllers/AI.controllers.ts
@@ -1,27 +1,31 @@
 import { Request, Response } from "express";
-import { OpenAI } from "openai";
+import Together from "together-ai";
 import asyncHandler from "../utils/asyncHandler";
 import "@dotenvx/dotenvx/config";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
+const together = new Together();
 export const askAssistant = asyncHandler(
   async (req: Request, res: Response) => {
     const { prompt, code } = req.body;
-
     try {
-      const response = await openai.chat.completions.create({
-        model: "gpt-4", // or 'gpt-3.5-turbo'
+      const response = await together.chat.completions.create({
+        model: "deepseek-ai/DeepSeek-V3",
         messages: [
           { role: "system", content: "You are a helpful coding assistant." },
           { role: "user", content: `${prompt}\n\n${code}` },
         ],
       });
-      console.log(response);
-      const answer = response.choices[0].message.content;
-      res.json({ answer });
+
+      if (
+        response.choices &&
+        response.choices.length > 0 &&
+        response.choices[0].message
+      ) {
+        const answer = response.choices[0].message.content;
+        res.json({ answer });
+      } else {
+        res.status(500).json({ error: "Failed to fetch AI response" });
+      }
     } catch (error: any) {
       console.error("AI error:", error.message);
       res.status(500).json({ error: "Failed to fetch AI response" });
@@ -33,8 +37,8 @@ export const askSuggestion = asyncHandler(
   async (req: Request, res: Response) => {
     const { code } = req.body;
     try {
-      const response = await openai.chat.completions.create({
-        model: "gpt-4", // or 'gpt-3.5-turbo'
+      const response = await together.chat.completions.create({
+        model: "deepseek-ai/DeepSeek-V3",
         messages: [
           {
             role: "system",
@@ -44,9 +48,17 @@ export const askSuggestion = asyncHandler(
           { role: "user", content: `${code}` },
         ],
       });
-      console.log(response);
-      const answer = response.choices[0].message.content;
-      res.json({ answer });
+
+      if (
+        response.choices &&
+        response.choices.length > 0 &&
+        response.choices[0].message
+      ) {
+        const answer = response.choices[0].message.content;
+        res.json({ answer });
+      } else {
+        res.status(500).json({ error: "Failed to fetch AI response" });
+      }
     } catch (error: any) {
       console.error("AI error:", error.message);
       res.status(500).json({ error: "Failed to fetch AI response" });
